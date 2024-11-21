ae398571173a09529d170d96eb3eca8f31f0ceec
Moshe Ekstein <mekstein@fidelitypayment.com>
2024-11-21 11:11:12 -0500

    Fix port binding for Render

--- api/process-payment.js
+++ api/process-payment.js
@@ -52,4 +52,7 @@ app.post('/api/process-payment', async (req, res) => {
   }
 });
 
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Custom server running on port ${PORT}`);
+});
